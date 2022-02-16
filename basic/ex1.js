8307c60b6bfd4b22284581df54c30f8b175cc83b
Dean Delic <deandelic6@gmail.com>
2022-02-16 15:03:26 +0100

    testing

---
+++ basic/ex1.js
@@ -0,0 +1,10 @@
+function crystalGazer(children, partners_name, geographic_location, job_title) {
+    var children = "2 children";
+    var partners_name = "Lisa";
+    var geographic_location = "Vienna";
+    var job_title = "Developer";
+    document.write("You will be" + (job_title) + "in" + (geographic_location) + "and merried to" + (partners_name) + "with" + (children))
+
+
+}
+crystalGazer()
